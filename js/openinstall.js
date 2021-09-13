288fd0fcba33a55e158fd7ab4192acdf023c7496
wade <hui@openinstall.io>
2021-09-13 17:02:23 +0800

    更新插件js

--- js/openinstall.js
+++ js/openinstall.js
@@ -2,16 +2,7 @@ document.addEventListener( "plusready",  function(){
     var _BARCODE = 'openinstall',
 		B = window.plus.bridge;
     var openinstall = {
-       /*
-       初始化前配置
-       options {
-           adEnabled: true, //SDK 需要获取广告追踪相关参数
-           macDisabled: true, //SDK 不需要获取 mac地址
-           imeiDisabled: true, //SDK 不需要获取 imei
-           gaid: "通过 google api 获取到的 advertisingId", //SDK 使用传入的gaid，不再获取gaid
-           oaid: "通过移动安全联盟获取到的 oaid", //SDK 使用传入的oaid，不再获取oaid
-       }
-       */
+
         configAndroid : function (options) {
             return B.exec(_BARCODE, "config", [options]);
         },
@@ -25,29 +16,28 @@ document.addEventListener( "plusready",  function(){
             return B.exec(_BARCODE, "config", [options]);
         },
 
-
         // 初始化
-        init : function (permission) {
-            return B.exec(_BARCODE, "init", [permission]);
+        init : function () {
+            return B.exec(_BARCODE, "init", []);
         },
 
-		//注册拉起回调
-		registerWakeUpHandler: function (successCallback, permission) {
-		    var success = typeof successCallback !== 'function' ? null : function(args) {
+        //注册拉起回调
+        registerWakeUpHandler: function (successCallback) {
+            var success = typeof successCallback !== 'function' ? null : function(args) {
                 successCallback(args);
             },
             callbackID = B.callbackId(success, null);
-            return B.exec(_BARCODE, "registerWakeUpHandler", [callbackID, permission]);
-		},
-		// 获取安装来源数据
-		getInstall : function (successCallback, timeout) {
-			var success = typeof successCallback !== 'function' ? null : function(args) {
-				successCallback(args);
-			},
-			callbackID = B.callbackId(success, null);
-			return B.exec(_BARCODE, "getInstall", [callbackID, timeout]);
-		},
-		// 注册上报
+            return B.exec(_BARCODE, "registerWakeUpHandler", [callbackID]);
+        },
+        // 获取安装来源数据
+        getInstall : function (successCallback, timeout) {
+            var success = typeof successCallback !== 'function' ? null : function(args) {
+                successCallback(args);
+            },
+            callbackID = B.callbackId(success, null);
+            return B.exec(_BARCODE, "getInstall", [callbackID, timeout]);
+        },
+        // 注册上报
         reportRegister : function () {
             return B.exec(_BARCODE, "reportRegister", []);
         },
